af06fae81a436e84dbfd1a9497018b7d9c03b13e
Zhanserik Kenes <zhans.kenes@gmail.com>
2016-10-07 12:26:27 +0600

    Initial Commit

---
+++ Payonline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Payonline
+//
+//  Created by Zhanserik on 10/7/16.
+//  Copyright © 2016 Kenes Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
